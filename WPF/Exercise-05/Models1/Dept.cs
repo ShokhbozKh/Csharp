3eeaa22f5b0d17e57482c4236e208903b1ae8f68
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 02:44:57 +0200

    Create Dept.cs

---
+++ WPF/Exercise-05/Models1/Dept.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_06.Models1
+{
+    public class Dept
+    {
+        public int Deptno { get; set; }
+        public string Dname { get; set; }
+        public string Loc { get; set; }
+    }
+}
