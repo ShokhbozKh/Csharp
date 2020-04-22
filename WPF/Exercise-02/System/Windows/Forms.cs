156a09b423a2e5a4dbcd7e327e5d917f4d897e4d
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 02:38:42 +0200

    Create Forms.cs

---
+++ WPF/Exercise-02/System/Windows/Forms.cs
@@ -0,0 +1,13 @@
+namespace System.Windows
+{
+    internal class Forms
+    {
+        internal class DialogResult
+        {
+            public static explicit operator DialogResult(MessageBoxResult v)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
