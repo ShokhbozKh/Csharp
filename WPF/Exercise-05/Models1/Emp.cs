9ed26b1ec6701b682f2b4a045fded2d6cd542244
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 02:45:13 +0200

    Create Emp.cs

---
+++ WPF/Exercise-05/Models1/Emp.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_06.Models1
+{
+    public class Emp
+    {
+        public int Empno { get; set; }
+        public string Ename { get; set; }
+        public int Sal { get; set; }
+        public int? Comm { get; set; }
+        public int Deptno { get; set; }
+        public int? Mgr { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Job { get; set; }
+    }
+}
