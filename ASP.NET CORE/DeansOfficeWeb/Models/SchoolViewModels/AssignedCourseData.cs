803dbdf7fcdb62367eb050e7a0ea51f1356dff55
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 16:07:55 +0200

    Add files via upload

---
+++ ASP.NET CORE/DeansOfficeWeb/Models/SchoolViewModels/AssignedCourseData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeansOfficeWeb.Models.SchoolViewModels
+{
+    public class AssignedCourseData
+    {
+        public int SubjectId { get; set; }
+        [Display (Name = "Choose Course")]
+        public string SubjectName { get; set; }
+        public List<Instructor> Instructors { get; set; }
+    }
+}
---
+++ ASP.NET CORE/DeansOfficeWeb/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DeansOfficeWeb.Models.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Subject> Subjects { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
