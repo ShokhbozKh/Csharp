85c3e949e279b719b99cf0608b2c2fe38ce87078
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 02:41:22 +0200

    Add files via upload

---
+++ WPF/Exercise-04/Models/Student_Subject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_05.Models
+{
+    class Student_Subject
+    {
+        public int IdStudentSubject { get; set; }
+        public int IdStudent { get; set; }
+        public int IdSubject { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ WPF/Exercise-04/Models/Studies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_05.Models
+{
+    class Studies
+    {
+        public int IdStudies { get; set; }
+        public string StudyName { get; set; }
+
+        public override string ToString()
+        {
+            return StudyName;
+        }
+    }
+}
