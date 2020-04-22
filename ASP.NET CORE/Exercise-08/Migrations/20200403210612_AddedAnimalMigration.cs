04a8a337ad19648da44169bc0b27ff120ffb7438
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 02:55:51 +0200

    Add files via upload

---
+++ ASP.NET CORE/Exercise-08/Migrations/20200403210612_AddedAnimalMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Exercise_08.Migrations
+{
+    public partial class AddedAnimalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AnimalName = table.Column<string>(maxLength: 15, nullable: false),
+                    Owner = table.Column<string>(maxLength: 150, nullable: false),
+                    Age = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
---
+++ ASP.NET CORE/Exercise-08/Migrations/20200403211526_AddedFkToAnimalTable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Exercise_08.Migrations
+{
+    public partial class AddedFkToAnimalTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Owner",
+                table: "Animals");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdOwner",
+                table: "Animals",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_IdOwner",
+                table: "Animals",
+                column: "IdOwner");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Animals_Owners_IdOwner",
+                table: "Animals",
+                column: "IdOwner",
+                principalTable: "Owners",
+                principalColumn: "IdOwner",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Animals_Owners_IdOwner",
+                table: "Animals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Animals_IdOwner",
+                table: "Animals");
+
+            migrationBuilder.DropColumn(
+                name: "IdOwner",
+                table: "Animals");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Owner",
+                table: "Animals",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
