8e76c69c6f7382e18d162d463a00572efce7883b
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 02:49:57 +0200

    Add files via upload

---
+++ WPF/Exercise-06/AddStudentDialog.xaml.cs
@@ -0,0 +1,216 @@
+﻿using Exercise_07.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Exercise_07
+{
+    /// <summary>
+    /// Interaction logic for AddStudentDialog.xaml
+    /// </summary>
+    public partial class AddStudentDialog : Window
+    {
+        private readonly int Id;    // for generating a new student
+        public AddStudentDialog(List<Study> studiesList, int newId)
+        {
+            InitializeComponent();
+            LoadStudiesData(studiesList);
+            this.Id = newId;
+        }
+
+        public AddStudentDialog(ref Student student, List<Study> studiesData)
+        {
+            InitializeComponent();
+            LoadStudiesData(studiesData);
+            this.Id = student.IdStudent;
+
+            FirstNameTextBox.Text = student.FirstName;
+            LastNameTextBox.Text = student.LastName;
+            AddressTextBox.Text = student.Address;
+            IndexNumberTextBox.Text = student.IndexNumber;
+            StudiesListBox.SelectedItem = student.Study;
+
+            AddButton.Content = "Edit";
+        }
+
+        public Student Student { get; set; }
+
+        private void LoadStudiesData(List<Study> studiesData)
+        {
+            StudiesListBox.ItemsSource = studiesData;
+        }
+
+        private void LastNameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!IsNameValid(LastNameTextBox.Text))
+            {
+                LastNameTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                LastNameTextBox.BorderBrush = SystemColors.ControlDarkBrush;
+            }
+        }
+
+        private void FirstNameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!IsNameValid(FirstNameTextBox.Text))
+            {
+                FirstNameTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                FirstNameTextBox.BorderBrush = SystemColors.ControlDarkBrush;
+            }
+        }
+
+        private void IndexNumberTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!IsIndexValid(IndexNumberTextBox.Text))
+            {
+                IndexNumberTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                IndexNumberTextBox.BorderBrush = SystemColors.ControlDarkBrush;
+            }
+        }
+
+        private void AddressTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!IsAddressValid(AddressTextBox.Text))
+            {
+                AddressTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                AddressTextBox.BorderBrush = SystemColors.ControlDarkBrush;
+            }
+
+        }
+
+        private void StudiesListBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            IsStudyValid();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckValidation())
+            {
+                Study study = (Study)StudiesListBox.SelectedItem;
+                Student newStudent = new Student
+                {
+                    IdStudent = this.Id,
+                    FirstName = char.ToUpper(FirstNameTextBox.Text[0]) + FirstNameTextBox.Text.Substring(1),
+                    LastName = char.ToUpper(LastNameTextBox.Text[0]) + LastNameTextBox.Text.Substring(1),
+                    Address = AddressTextBox.Text,
+                    IndexNumber = IndexNumberTextBox.Text,
+                    IdStudies = study.IdStudies,
+                    Study = study
+                };
+
+                this.Student = newStudent;
+
+                Close();
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // Validate fields
+        private bool CheckValidation()
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            string index = IndexNumberTextBox.Text;
+            string address = AddressTextBox.Text;
+
+            if (!IsNameValid(firstName))
+            {
+                MessageBox.Show("No digits and symbols are allowed", "Please, insert valid first name");
+                return false;
+            }
+            else if (!IsNameValid(lastName))
+            {
+                MessageBox.Show("No digits and symbols are allowed", "Please, insert valid last name");
+                return false;
+            }
+            else if (!IsAddressValid(address))
+            {
+                MessageBox.Show("Please, insert valid address"); ;
+                return false;
+            }
+            else if (!IsIndexValid(index))
+            {
+                MessageBox.Show("example: s12345", "Please, insert a valid index number");
+                return false;
+            }
+            else if (!IsStudyValid())
+            {
+                MessageBox.Show("Please, select one study academic specialization");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        // Is name is for first and last names
+        private bool IsNameValid(string name)
+        {
+            if (name.Any(char.IsDigit) || name.Any(char.IsSymbol) || name.Equals(null) || name.Equals(" ") || name.Equals(""))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        // index should consist of small letter 's' and 5 digits
+        private bool IsIndexValid(string index)
+        {
+            if (!index.StartsWith("s") || index.Length != 6 || index.Substring(1).Any(char.IsLetter))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool IsAddressValid(string address)
+        {
+            if (address.Equals(null) || address.Equals(""))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool IsStudyValid()
+        {
+            if (StudiesListBox.SelectedItems.Count != 1)
+            {
+                StudiesListBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                return false;
+            }
+            else
+            {
+                StudiesListBox.BorderBrush = SystemColors.ControlDarkBrush;
+                return true;
+            }
+        }
+    }
+}
---
+++ WPF/Exercise-06/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using Exercise_07.DAL;
+using Exercise_07.Models;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Exercise_07
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            StudentsDataGrid.ItemsSource = StudentDbService.GetStudents();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // track on selection number of students on datagrid
+        private void StudentsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int studentsSelected = StudentsDataGrid.SelectedItems.Count;
+
+            if(studentsSelected > 1)
+            {
+                ChosenStudents.Content = $"You chose {studentsSelected} students";
+            } else if(studentsSelected == 1)
+            {
+                ChosenStudents.Content = $"You chose 1 student";
+            }
+            
+        }
+
+        // Show add student dialog with update option on double click
+        private void StudentsDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Student studentToUpdate = (Student) StudentsDataGrid.SelectedItem;
+            var updateDialog = new AddStudentDialog(ref studentToUpdate, StudentDbService.GetStudies());
+
+            if (updateDialog.ShowDialog() == false)
+            {
+                Student student = updateDialog.Student;
+                //int g = 0;
+                if(student != null)
+                {
+                    StudentDbService.UpdateStudent(student);
+                    MessageBox.Show("Student was successfully edited!");
+                }
+            }
+        }
+
+        // Add button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /* Add generate Id because students are searched through list, so when they added to list directly and not
+             * retrieved from db, they don't have id, and you won't be able to find them and update instantly after adding
+             */
+            var addDialog = new AddStudentDialog(StudentDbService.GetStudies(), StudentDbService.GenerateId());
+
+            if(addDialog.ShowDialog() == false)
+            {
+                if(addDialog.Student != null)
+                {
+                    StudentDbService.AddStudent(addDialog.Student);
+                    MessageBox.Show("Student was successfully added");
+                }
+            }
+        }
+
+        // Delete button
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int selectedStudents = StudentsDataGrid.SelectedItems.Count;
+            if(selectedStudents > 1)
+            {
+                if((MessageBox.Show($"Are you sure you want to delete {selectedStudents} students? ", $"{selectedStudents} students are to be deleted", MessageBoxButton.YesNo) == MessageBoxResult.Yes))
+                {
+                    // list is needed because cannot pass selected item of datagrid to the method remove directly as an obj
+                    List<Student> studentsToRemove = new List<Student>();
+                   
+                    for (int i = 0; i < StudentsDataGrid.SelectedItems.Count; i++)
+                    {
+                        // add each selected student to the list
+                        studentsToRemove.Add((Student)StudentsDataGrid.SelectedItems[i]);
+                        // StudentDbService.RemoveStudentFromDb((Student) StudentsDataGrid.SelectedItems[i]); --> doesnt work
+                    }
+
+                    foreach (Student st in studentsToRemove)
+                    {
+                        // pass each student to remove method
+                        StudentDbService.RemoveStudent(st);
+                    }
+
+                    MessageBox.Show("Students were successfully deleted!");
+                }
+            }
+            else if(selectedStudents == 1)
+            {
+                if ((MessageBox.Show("Are you sure you want to delete ?", $" {StudentsDataGrid.SelectedItem.ToString()} to be deleted", MessageBoxButton.YesNo) == MessageBoxResult.Yes))
+                {
+                    StudentDbService.RemoveStudent((Student)StudentsDataGrid.SelectedItem);
+                }
+                MessageBox.Show("Student was successfully deleted");
+            } else if(selectedStudents == 0)
+            {
+                MessageBox.Show("Please, select at least one student!");
+            }
+            
+        }
+    }
+}
+
+/*
+ * Student studentToUpdate = (Student)StudentsDataGrid.SelectedItem;
+            var updateDialog = new AddStudentDialog(studentToUpdate, StudentDbService.GetStudies());
+            
+            if(updateDialog.ShowDialog() == false)
+            {
+                Student student = updateDialog.Student;
+                //int g = 0;
+                StudentDbService.UpdateStudent(studentToUpdate, student);
+            }
+*/
