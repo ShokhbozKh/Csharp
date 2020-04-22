ead9e84b21801c14abf8d4a48b2b89ba0cce7816
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 16:09:16 +0200

    Add files via upload

---
+++ ASP.NET CORE/DeansOfficeWeb/Controllers/StudiesController.cs
@@ -0,0 +1,110 @@
+﻿using DeansOfficeWeb.DAL;
+using DeansOfficeWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DeansOfficeWeb.Controllers
+{
+    public class StudiesController : Controller
+    {
+        public readonly IDbLayer _context;
+
+        public StudiesController(IDbLayer context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Studies = await _context.GetStudies();
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var study = await _context.GetStudy(id);
+
+            if (study == null)
+            {
+                return NotFound();
+            }
+
+            return View(study);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Study study)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", study);
+            }
+
+            await _context.AddStudy(study);
+
+            return RedirectToAction("Create");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var study = await _context.GetStudy(id);
+
+            if (study == null)
+            {
+                return NotFound();
+            }
+
+            return View(study);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, [Bind("IdStudy, Name, Duration, Price")] Study studyToEdit)
+        {
+            if(id != studyToEdit.IdStudy)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", studyToEdit);
+            }
+
+            await _context.EditStudy(studyToEdit);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var study = await _context.GetStudy(id);
+
+            return View(study);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id, int d)
+        {
+            await _context.DeleteStudy(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ ASP.NET CORE/DeansOfficeWeb/Controllers/SubjectsController.cs
@@ -0,0 +1,99 @@
+﻿using DeansOfficeWeb.DAL;
+using DeansOfficeWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DeansOfficeWeb.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        public readonly IDbLayer _context;
+
+        public SubjectsController(IDbLayer context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Subjects = await _context.GetSubjects();
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var subject = await _context.GetSubject(id);
+
+            return View(subject);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Subject subject)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", subject);
+            }
+
+            await _context.AddSubject(subject);
+
+            return RedirectToAction("Create");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.GetSubject(id);
+
+            if(subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, [Bind("IdSubject, Name, Hours, Price, Credits")] Subject subjectToEdit)
+        {
+            if(id != subjectToEdit.IdSubject)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", subjectToEdit);
+            }
+
+            await _context.EditSubject(subjectToEdit);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var subject = await _context.GetSubject(id);
+
+            return View(subject);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id, int g)
+        {
+            await _context.DeleteSubject(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
