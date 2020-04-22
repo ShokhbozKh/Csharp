ee935492735ec42e59121656dfc1fd60fa8c64b0
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 02:48:34 +0200

    Add files via upload

---
+++ WPF/Exercise-06/Models/Study.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace Exercise_07.Models
+{
+    [Table("apbd.Studies")]
+    public partial class Study
+    {
+        [Key]
+        public int IdStudies { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
