aa9af0134fb2b82b8f7a9dc310df12e7346cd210
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 16:06:56 +0200

    Add files via upload

---
+++ ASP.NET CORE/DeansOfficeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeansOfficeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASP.NET CORE/DeansOfficeWeb/Models/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeansOfficeWeb.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int IdSubject { get; set; }
+        [Required, MaxLength(100)]
+        [Display(Name = "Subject Name")]
+        public string Name { get; set; }
+
+        [Range(1, 10000)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Display(Name = "Subject price")]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Duration)]
+        [Display(Name = "Hours of classes")]
+        public int Hours { get; set; }
+
+        [Range(1, 60)]
+        [Display(Name = "ECTS")]
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
