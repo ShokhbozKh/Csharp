9d57341335f5718ba68b3624c2e60c43dd1b003a
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-04-22 15:59:43 +0200

    Add files via upload

---
+++ ASP.NET CORE/Exercise-10/Migrations/20200405145936_AddedPlayerTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Exercise_10.Migrations
+{
+    public partial class AddedPlayerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    IdPlayer = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(maxLength: 150, nullable: false),
+                    BirthDate = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.IdPlayer);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
---
+++ ASP.NET CORE/Exercise-10/Migrations/20200405151051_AddedTeamTable.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Exercise_10.Migrations
+{
+    public partial class AddedTeamTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdTeam",
+                table: "Players",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamIdTeam",
+                table: "Players",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    IdTeam = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.IdTeam);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamIdTeam",
+                table: "Players",
+                column: "TeamIdTeam");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamIdTeam",
+                table: "Players",
+                column: "TeamIdTeam",
+                principalTable: "Teams",
+                principalColumn: "IdTeam",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamIdTeam",
+                table: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_TeamIdTeam",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "IdTeam",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TeamIdTeam",
+                table: "Players");
+        }
+    }
+}
