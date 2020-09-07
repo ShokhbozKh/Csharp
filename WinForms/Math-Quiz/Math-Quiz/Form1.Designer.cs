84f153ca31524674d872955362f1f7be5993391b
Miraziz <khidoyatov1997@gmail.com>
2020-09-07 10:22:36 +0200

    added add, substract, divide, multiply funcitonalities

--- WinForms/Math-Quiz/Math-Quiz/Form1.Designer.cs
+++ WinForms/Math-Quiz/Math-Quiz/Form1.Designer.cs
@@ -29,12 +29,323 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.plusRightLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.NumericUpDown();
+            this.difference = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.minusRightLabel = new System.Windows.Forms.Label();
+            this.minusLeftLabel = new System.Windows.Forms.Label();
+            this.product = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timesRightLabel = new System.Windows.Forms.Label();
+            this.timesLeftLabel = new System.Windows.Forms.Label();
+            this.quotient = new System.Windows.Forms.NumericUpDown();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.divideRightLabel = new System.Windows.Forms.Label();
+            this.divideLeftLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(279, 2);
+            this.timeLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(233, 34);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = " ";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time Left";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // plusLeftLabel
+            // 
+            this.plusLeftLabel.Location = new System.Drawing.Point(49, 52);
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            this.plusLeftLabel.Size = new System.Drawing.Size(70, 58);
+            this.plusLeftLabel.TabIndex = 2;
+            this.plusLeftLabel.Text = "?";
+            this.plusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusRightLabel
+            // 
+            this.plusRightLabel.Location = new System.Drawing.Point(206, 52);
+            this.plusRightLabel.Name = "plusRightLabel";
+            this.plusRightLabel.Size = new System.Drawing.Size(70, 58);
+            this.plusRightLabel.TabIndex = 3;
+            this.plusRightLabel.Text = "?";
+            this.plusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(130, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 58);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "+";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(282, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 58);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "=";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(358, 65);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(120, 35);
+            this.sum.TabIndex = 2;
+            this.sum.ValueChanged += new System.EventHandler(this.Check_Sum);
+            this.sum.Enter += new System.EventHandler(this.Answer_Enter);
+            // 
+            // difference
+            // 
+            this.difference.Location = new System.Drawing.Point(358, 123);
+            this.difference.Name = "difference";
+            this.difference.Size = new System.Drawing.Size(120, 35);
+            this.difference.TabIndex = 3;
+            this.difference.ValueChanged += new System.EventHandler(this.Check_Difference);
+            this.difference.Enter += new System.EventHandler(this.Answer_Enter);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(282, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 58);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "=";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(130, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 58);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "-";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minusRightLabel
+            // 
+            this.minusRightLabel.Location = new System.Drawing.Point(206, 110);
+            this.minusRightLabel.Name = "minusRightLabel";
+            this.minusRightLabel.Size = new System.Drawing.Size(70, 58);
+            this.minusRightLabel.TabIndex = 8;
+            this.minusRightLabel.Text = "?";
+            this.minusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minusLeftLabel
+            // 
+            this.minusLeftLabel.Location = new System.Drawing.Point(49, 110);
+            this.minusLeftLabel.Name = "minusLeftLabel";
+            this.minusLeftLabel.Size = new System.Drawing.Size(70, 58);
+            this.minusLeftLabel.TabIndex = 7;
+            this.minusLeftLabel.Text = "?";
+            this.minusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // product
+            // 
+            this.product.Location = new System.Drawing.Point(358, 174);
+            this.product.Name = "product";
+            this.product.Size = new System.Drawing.Size(120, 35);
+            this.product.TabIndex = 4;
+            this.product.ValueChanged += new System.EventHandler(this.Check_Product);
+            this.product.Enter += new System.EventHandler(this.Answer_Enter);
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(282, 161);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 58);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "=";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(130, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 58);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "x";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timesRightLabel
+            // 
+            this.timesRightLabel.Location = new System.Drawing.Point(206, 161);
+            this.timesRightLabel.Name = "timesRightLabel";
+            this.timesRightLabel.Size = new System.Drawing.Size(70, 58);
+            this.timesRightLabel.TabIndex = 13;
+            this.timesRightLabel.Text = "?";
+            this.timesRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timesLeftLabel
+            // 
+            this.timesLeftLabel.Location = new System.Drawing.Point(49, 161);
+            this.timesLeftLabel.Name = "timesLeftLabel";
+            this.timesLeftLabel.Size = new System.Drawing.Size(70, 58);
+            this.timesLeftLabel.TabIndex = 12;
+            this.timesLeftLabel.Text = "?";
+            this.timesLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // quotient
+            // 
+            this.quotient.Location = new System.Drawing.Point(358, 232);
+            this.quotient.Name = "quotient";
+            this.quotient.Size = new System.Drawing.Size(120, 35);
+            this.quotient.TabIndex = 5;
+            this.quotient.ValueChanged += new System.EventHandler(this.Check_Quotient);
+            this.quotient.Enter += new System.EventHandler(this.Answer_Enter);
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(282, 219);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 58);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "=";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(130, 219);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(70, 58);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "/";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // divideRightLabel
+            // 
+            this.divideRightLabel.Location = new System.Drawing.Point(206, 219);
+            this.divideRightLabel.Name = "divideRightLabel";
+            this.divideRightLabel.Size = new System.Drawing.Size(70, 58);
+            this.divideRightLabel.TabIndex = 18;
+            this.divideRightLabel.Text = "?";
+            this.divideRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // divideLeftLabel
+            // 
+            this.divideLeftLabel.Location = new System.Drawing.Point(49, 219);
+            this.divideLeftLabel.Name = "divideLeftLabel";
+            this.divideLeftLabel.Size = new System.Drawing.Size(70, 58);
+            this.divideLeftLabel.TabIndex = 17;
+            this.divideLeftLabel.Text = "?";
+            this.divideLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.AutoSize = true;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(176, 303);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 42);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start Quiz";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(553, 357);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.quotient);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.divideRightLabel);
+            this.Controls.Add(this.divideLeftLabel);
+            this.Controls.Add(this.product);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.timesRightLabel);
+            this.Controls.Add(this.timesLeftLabel);
+            this.Controls.Add(this.difference);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.minusRightLabel);
+            this.Controls.Add(this.minusLeftLabel);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.plusRightLabel);
+            this.Controls.Add(this.plusLeftLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.Label plusRightLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown sum;
+        private System.Windows.Forms.NumericUpDown difference;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label minusRightLabel;
+        private System.Windows.Forms.Label minusLeftLabel;
+        private System.Windows.Forms.NumericUpDown product;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label timesRightLabel;
+        private System.Windows.Forms.Label timesLeftLabel;
+        private System.Windows.Forms.NumericUpDown quotient;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label divideRightLabel;
+        private System.Windows.Forms.Label divideLeftLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WinForms/Math-Quiz/Math-Quiz/Form1.cs
+++ WinForms/Math-Quiz/Math-Quiz/Form1.cs
@@ -12,9 +12,190 @@ namespace Math_Quiz
 {
     public partial class Form1 : Form
     {
+        // To generate random numbers
+        Random randomizer = new Random();
+
+        // For addition problems
+        int add1;
+        int add2;
+
+        // For substraction problems
+        int sub1;
+        int sub2;
+
+        // For multiplication problems
+        int mul1;
+        int mul2;
+
+        // For division problems
+        int div1;
+        int div2;
+
+        // Timer tracker
+        int timeLeft;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Begin the quiz
+        public void StartQuiz()
+        {
+            // Set values for the addition part
+            add1 = randomizer.Next(51);
+            add2 = randomizer.Next(51);
+
+            // Set values of the Add labels
+            plusLeftLabel.Text = add1.ToString();
+            plusRightLabel.Text = add2.ToString();
+
+            // Set the initial answer to 0
+            sum.Value = 0;
+
+            // Set values for the substraction part
+            sub1 = randomizer.Next(1, 101);
+            sub2 = randomizer.Next(1, sub1);
+
+            // Set values of the Substract labels
+            minusLeftLabel.Text = sub1.ToString();
+            minusRightLabel.Text = sub2.ToString();
+
+            // Set the initial answer to 0
+            difference.Value = 0;
+
+            // Set values for the multiplication part
+            mul1 = randomizer.Next(2, 11);
+            mul2 = randomizer.Next(2, 11);
+
+            // Set values for the Multiplication Labels
+            timesLeftLabel.Text = mul1.ToString();
+            timesRightLabel.Text = mul2.ToString();
+
+            // Set the initial answer to 0
+            product.Value = 0;
+
+            // Set values for the division part
+            // get the value of divisor first
+            // and multiply random by it for divinded
+            div2 = randomizer.Next(2, 11);
+            div1 = randomizer.Next(2, 11) * div2;
+
+            // Set values for the Division Lables
+            divideLeftLabel.Text = div1.ToString();
+            divideRightLabel.Text = div2.ToString();
+
+            // Set the initial answer to 0
+            quotient.Value = 0;
+
+            // Start the timer
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+        }
+
+        // Check the answer
+        public bool CheckTheAnswer()
+        {
+            if ((add1 + add2 == sum.Value)
+                && (sub1 - sub2 == difference.Value)
+                && (mul1 * mul2 == product.Value)
+                && (div1 / div2 == quotient.Value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            StartQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                // If answer is correct, stop the quiz
+                // and show message box
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Conguratilations");
+                startButton.Enabled = true;
+            }
+            else if(timeLeft > 0)
+            {
+                // Display new time left
+                // update the Time Left Label
+                timeLeft -= 1;
+                timeLabel.Text = $"{timeLeft} seconds";
+
+                // If less than 5 seconds left
+                // set borders to red
+                if(timeLeft < 5)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer,
+                // show message box and fill the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = add1 + add2;
+                difference.Value = sub1 - sub2;
+                product.Value = mul1 * mul2;
+                quotient.Value = div1 / div2;
+                startButton.Enabled = true;
+                timeLabel.BackColor = Color.White;
+            }
+
+        }
+
+        private void Answer_Enter(object sender, EventArgs e)
+        {
+            if(sender is NumericUpDown answerBox)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);            
+            }
+        }
+
+        private void Check_Sum(object sender, EventArgs e)
+        {
+            if (add1 + add2 == sum.Value)
+                sum.BackColor = Color.Green;
+            else
+                sum.BackColor = Color.Red;
+        }
+
+        private void Check_Difference(object sender, EventArgs e)
+        {
+            if (sub1 - sub2 == difference.Value)
+                difference.BackColor = Color.Green;
+            else
+                difference.BackColor = Color.Red;
+        }
+
+        private void Check_Product(object sender, EventArgs e)
+        {
+            if (mul1 * mul2 == product.Value)
+                product.BackColor = Color.Green;
+            else
+                product.BackColor = Color.Red;
+        }
+
+        private void Check_Quotient(object sender, EventArgs e)
+        {
+            if (div1 / div2 == quotient.Value)
+                quotient.BackColor = Color.Green;
+            else
+                quotient.BackColor = Color.Red;
         }
     }
 }
