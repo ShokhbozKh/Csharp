a41f2a7cb4ac35c4b1e4aa7c503500e605205066
Miraziz <khidoyatov1997@gmail.com>
2020-09-07 11:38:50 +0200

    added icon assigning to labels

--- WinForms/Matching-Game/Form1.cs
+++ WinForms/Matching-Game/Form1.cs
@@ -12,9 +12,39 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Random randomizer = new Random();
+
+        List<string> icons = new List<string>
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
+
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            // Assign random value from icons
+            // to each Label
+
+            foreach(Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Label iconLabel)
+                {
+                    // Get random number between the length
+                    // of the Icons collection
+
+                    int random = randomizer.Next(icons.Count);
+                    iconLabel.Text = icons[random];
+
+                    icons.RemoveAt(random);
+                }
+            }
         }
     }
 }
