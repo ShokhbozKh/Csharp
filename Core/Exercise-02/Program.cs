4fd23c2779e337f070aed777c8e606a116caa57b
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-10-18 14:13:00 +0200

    added exercise-02

--- Core/Exercise-02/Program.cs
+++ Core/Exercise-02/Program.cs
@@ -6,7 +6,33 @@ namespace Exercise_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), el => Convert.ToInt32(el));
+
+            int currentSequense = 1;
+            int maxSequense = 0;
+            int previousNumber = numbers[0];
+            int maxNumber = numbers[0];
+
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if(previousNumber == numbers[i])
+                {
+                    currentSequense++;
+                    if(currentSequense > maxSequense)
+                    {
+                        maxSequense = currentSequense;
+                        maxNumber = numbers[i];
+                    }
+                }
+                else
+                {
+                    currentSequense = 1;
+                }
+
+                previousNumber = numbers[i];
+            }
+
+            Console.WriteLine($"Longest sequence: {maxSequense} times {maxNumber}");
         }
     }
 }
