2f32d4da1c340b0b28ad07ec19bde2bb9a2b25b5
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-10-21 22:58:49 +0200

    added exercise-05

--- Core/Exercise-05/Program.cs
+++ Core/Exercise-05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
 
 namespace Exercise_05
 {
@@ -6,7 +7,39 @@ namespace Exercise_05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] arr =
+            {
+                new int[] {1,2,3,4,5,6},
+                new int[] {2,3,4,5,6,7},
+                new int[] {3,4,5,6,7,8},
+                new int[] {4,5,6,7,8,9},
+                new int[] {5,6,7,8,9,10}
+            };
+            Inner(arr);
+            
+            foreach (int[] subArray in Inner(arr))
+            {
+                foreach (int el in subArray)
+                    Console.Write($"{el} ");
+
+                Console.WriteLine();
+            }
+        }
+
+        static int[][] Inner(int[][] arr)
+        {
+            int[][] result = new int[arr.Length-2][];
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                result[i] = new int[arr[i].Length - 2];
+                for(int j = 0; j < result[i].Length; j++)
+                {
+                    result[i][j] = arr[i + 1][j + 1];
+                }
+            }
+
+            return result;
         }
     }
 }
