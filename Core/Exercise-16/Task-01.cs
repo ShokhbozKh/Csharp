235f5e229c1a1816fd763c233617cb05990148c8
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-10-25 12:00:27 +0100

    added exercise-16

---
+++ Core/Exercise-16/Task-01.cs
@@ -0,0 +1,69 @@
+﻿namespace Exercise_16
+{
+    public class ParentStack
+    {
+        Node head;
+
+        public void Push(BracketType bracketType)
+        {
+            Node newNode = new Node(bracketType);
+
+            if (head is null)
+            {
+                head = newNode;
+                return;
+            }
+
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public BracketType Pop()
+        {
+            if (Empty())
+                return BracketType.Default;
+
+            BracketType popedData = head.Data;
+
+            head = head.Next;
+
+            return popedData;
+        }
+
+        public BracketType Peek()
+        {
+            return head.Data;
+        }
+
+        public bool Empty()
+        {
+            return head is null;
+        }
+    }
+
+    class Node
+    {
+        public BracketType Data { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(BracketType bracketType)
+        {
+            this.Data = bracketType;
+        }
+
+        public Node(BracketType bracketType, Node next) : this(bracketType)
+        {
+            Next = next;
+        }
+    }
+
+    public enum BracketType
+    {
+        LeftSquare = '[', RightSquare = ']',
+        LeftCurly = '{', RightCurly = '}',
+        LeftRound = '(', RightRound = ')',
+        LeftAngle = '<', RightAngle = '>',
+        Default
+    }
+}
---
+++ Core/Exercise-16/Task-03.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Exercise_16
+{
+    internal class Purchase : IComparer<Purchase>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Purchase(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+        public int Compare([AllowNull] Purchase x, [AllowNull] Purchase y)
+        {
+            if (x.Name.CompareTo(y.Name) == 0)
+                return 0;
+            else if (x.Name.CompareTo(y.Name) > 0)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
