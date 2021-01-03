6b960830aade76d0b25186ba3d36b6726bbad086
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-01-03 22:47:55 +0100

    added history and category models

---
+++ OOP/Assignment-05/Assignment-05/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_05.Models
+{
+    public enum ItemType
+    {
+        Income,
+        Expense
+    }
+
+    public class Category
+    {
+        [Key]
+        public int IdCategory { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Name { get; set; }
+
+        public ItemType CategoryType { get; set; }
+        public List<Item> Items { get; } = new List<Item>();
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ OOP/Assignment-05/Assignment-05/Models/History.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment_05.Models
+{
+    public class History
+    {
+        [Key]
+        public int IdHistory { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DeletedDate { get; set; }
+
+        [Required, MaxLength(150)]
+        [Display(Name = "Comment")]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        [Display(Name = "Total Sum")]
+        public decimal TotalSum { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [Display(Name = "Category")]
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public string ItemType { get; set; }
+    }
+}
