b051b78a2110cd0dcf40e50d7f69ed30244cec66
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-09 00:37:43 +0100

    added password helper

--- OOP/Final-Project/FinalProject/FinalProject/MainWindow.xaml.cs
+++ OOP/Final-Project/FinalProject/FinalProject/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FinalProject.Views;
 
 namespace FinalProject
 {
@@ -23,6 +24,7 @@ namespace FinalProject
         public MainWindow()
         {
             InitializeComponent();
+            new LoginDialog().Show();
         }
     }
 }
