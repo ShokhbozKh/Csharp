002468044cf9f7422ec9bca90dc0ef91f361a302
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-10 21:26:18 +0100

    added payment control

---
+++ OOP/Final-Project/FinalProject/FinalProject/Views/PaymetControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalProject.Views
+{
+    /// <summary>
+    /// Interaction logic for PaymetControl.xaml
+    /// </summary>
+    public partial class PaymetControl : UserControl
+    {
+        public PaymetControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
