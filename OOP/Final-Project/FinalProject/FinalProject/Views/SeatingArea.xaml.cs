d6f22ea8664f4ecb01b6469deef70ebb32d483eb
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-10 13:20:57 +0100

    added seating area control

---
+++ OOP/Final-Project/FinalProject/FinalProject/Views/SeatingArea.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalProject.Views
+{
+    /// <summary>
+    /// Interaction logic for SeatingArea.xaml
+    /// </summary>
+    public partial class SeatingArea : UserControl
+    {
+        public ObservableCollection<int> Seats { get; set; }
+        public SeatingArea()
+        {
+            LoadData();
+            InitializeComponent();
+        }
+
+        void LoadData()
+        {
+            Seats = new ObservableCollection<int>();
+
+            for (int i = 0; i < 20; i++) Seats.Add(i);
+
+        }
+    }
+}
