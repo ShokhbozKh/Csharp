a3cf59fb57a70fa5331a07cbd329e122d1f5cfa0
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-11 23:57:26 +0100

    add initial db layer

---
+++ OOP/Final-Project/FinalProject/FinalProject/DAL/IDbLayer.cs
@@ -0,0 +1,11 @@
+﻿using FinalProject.Models;
+using System.Threading.Tasks;
+
+namespace FinalProject.DAL
+{
+    public interface IDbLayer
+    {
+        Task<Enumerable<User>> GetUsers();
+        Task<Enumerable<Customer>> GetCustomers();
+    }
+}
