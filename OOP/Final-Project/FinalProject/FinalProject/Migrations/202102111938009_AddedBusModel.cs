687d4830ab085bcd3730bb05a54805fd08222bf6
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-11 20:38:35 +0100

    bus employee models migration

---
+++ OOP/Final-Project/FinalProject/FinalProject/Migrations/202102111938009_AddedBusModel.cs
@@ -0,0 +1,31 @@
+﻿namespace FinalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedBusModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Buses",
+                c => new
+                    {
+                        IdBus = c.Int(nullable: false, identity: true),
+                        BusName = c.String(),
+                        SeatCount = c.Int(nullable: false),
+                        HasWifi = c.Boolean(nullable: false),
+                        HasAirCondition = c.Boolean(nullable: false),
+                        BusTypeId = c.Int(nullable: false),
+                        BusType = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdBus);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Buses");
+        }
+    }
+}
