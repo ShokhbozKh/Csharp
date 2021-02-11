cc344e865c0671892b08314a0f1a8f68f9d71a61
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-11 16:29:49 +0100

    add customer & customertype models

---
+++ OOP/Final-Project/FinalProject/FinalProject/Models/CustomerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public enum CustomerTypes
+    {
+        Loyal = 0
+        Student = 1,
+    }
+}
