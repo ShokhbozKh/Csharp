c71f9352ad442d139204378d0e053910cea36df7
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-11 16:00:48 +0100

    add user model

---
+++ OOP/Final-Project/FinalProject/FinalProject/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalProject.Models
+{
+    abstract class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+
+        public abstract void BookTicket();
+    }
+}
