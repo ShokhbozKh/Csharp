c5dff87d35d669d4889cc7ea5f061d8855b1a920
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-02-26 19:52:11 +0100

    initial commit

---
+++ ASP.NET CORE/BooksAPI/BooksAPI/Models/IBookstoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksAPI.Models
+{
+    interface IBookstoreDatabaseSettings
+    {
+        string BooksCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
