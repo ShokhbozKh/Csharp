56ad5d7e6ccc82c806c20b122a8a41acc1947390
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-04-11 11:11:37 +0200

    add navigation links color change

--- ASP.NET CORE/RazorPagesMovie/RazorPagesMovie/wwwroot/js/site.js
+++ ASP.NET CORE/RazorPagesMovie/RazorPagesMovie/wwwroot/js/site.js
@@ -2,4 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const navToMovieBtn = document.getElementById('navMovieBtn');
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.nav-link').forEach(link => {
+        if (link.getAttribute('href').toLowerCase() === location.pathname.toLowerCase()) {
+            link.classList.add('active-link');
+        } else {
+            link.classList.remove('active-link');
+        }
+    });
+})
