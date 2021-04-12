245f2dfbc79ddd0bfe45ed00605d7be1579cc36b
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-04-12 11:00:50 +0200

    add db initializer, dependecy injection

--- ASP.NET CORE/DeansOffice/DeansOffice/Program.cs
+++ ASP.NET CORE/DeansOffice/DeansOffice/Program.cs
@@ -1,11 +1,10 @@
+using System;
+using DeansOffice.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace DeansOffice
 {
@@ -13,7 +12,28 @@ namespace DeansOffice
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<SchoolContext>();
+                Seed.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during database creation");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
