cb2a074c088affae875d462f108d0454d2429b07
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-04-12 10:34:13 +0200

    dean's office initial commit

---
+++ ASP.NET CORE/DeansOffice/DeansOffice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeansOffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
