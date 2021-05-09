a9faf8d9434251758b59c0a516880646884e7b10
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-05-09 18:47:08 +0200

    add dynamic inheritance

---
+++ OOP/Assignment-03/Assignment-03/Dynamic/Company.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment_03.Dynamic
+{
+    class Company
+    {
+        public string Name { get; private set; }
+        public decimal Revenue { get; private set; }
+
+        public List<Employee> Employees { get; private set; } = new List<Employee>();
+
+        public Company(string name, decimal revenue)
+        {
+            Name = name;
+            Revenue = revenue;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employee == null) throw new ArgumentNullException();
+
+            if (Employees.Contains(employee)) throw new Exception($"The company {Name} already has given employee");
+
+            Employees.Add(employee);
+        }
+
+        public void RemoveEmployee(Employee employee)
+        {
+            if (employee == null) throw new ArgumentNullException();
+
+            if (Employees.Contains(employee)) throw new Exception($"The company {Name} does not contain given employee");
+
+            Employees.Remove(employee);
+        }
+    }
+}
---
+++ OOP/Assignment-03/Assignment-03/Dynamic/Limited.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment_03.Dynamic
+{
+    class Limited
+    {
+        public decimal VAT { get; private set; }
+
+        public Limited(decimal vat)
+        {
+            VAT = vat;
+        }
+    }
+}
